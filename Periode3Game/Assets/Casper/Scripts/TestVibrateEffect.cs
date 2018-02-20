bdefcc4ef962930589ed6d81e9109b5e1a2e6997
Stefan <stefanherder2001@gmail.com>
2018-02-20 15:43:26 +0100

    Added Vibration

---
+++ Periode3Game/Assets/Casper/Scripts/TestVibrateEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class TestVibrateEffect : MonoBehaviour {
+
+    public float s;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    bool playerIndexSet = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 4; ++i)
+        {
+            PlayerIndex testPlayerIndex = (PlayerIndex)i;
+            GamePadState testState = GamePad.GetState(testPlayerIndex);
+            if (testState.IsConnected)
+            {
+                Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                playerIndex = testPlayerIndex;
+                playerIndexSet = true;
+            }
+        }
+        GamePad.SetVibration(playerIndex, s, s);
+        if (Input.GetButtonDown("DPadUpDown"))
+        {
+
+        }
+	}
+}
