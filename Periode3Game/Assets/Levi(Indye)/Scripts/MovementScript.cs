6ec4e729d064c9b9e312bb4144504eeb454c4639
Levindye <33858994+Levindye@users.noreply.github.com>
2018-03-12 14:33:59 +0100

    Started Overworld

---
+++ Periode3Game/Assets/Levi(Indye)/Scripts/MovementScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+    public float speed;
+    public Vector3 move;
+    public float x;
+    public float z;
+    public Rigidbody r;
+    public bool b;
+    public Vector3 height;
+    public Vector3 view;
+    public Vector3 camlook;
+    public Vector3 reset;
+    public GameObject player;
+    public GameObject cam;
+    public GameObject button1;
+    public GameObject button2;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(move * Time.deltaTime * speed);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+        }
+        
+        if (Input.GetButton("Escape"))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            button1.SetActive(true);
+            button2.SetActive(true);
+            player.SetActive(false);
+            cam.SetActive(true);
+        }
+        x = Input.GetAxis("Vertical");
+        z = Input.GetAxis("Horizontal");
+        move.x = z;
+        move.z = x;
+        if (b == true)
+        {
+            if (Input.GetButtonDown("Jump") == true)
+            {
+                r.velocity = height;
+                b = false;
+            }
+        }
+        if (Input.GetButtonDown("Shift"))
+        {
+            speed *= 2f;
+        }
+        if (Input.GetButtonUp("Shift"))
+        {
+            speed /= 2f;
+        }
+        if (transform.position.y <= -20)
+        {
+            reset.y = 10;
+            transform.position = reset;
+            print(transform.position);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        b = true;
+    }
+  
+}
+
+
---
+++ Periode3Game/Assets/Levi(Indye)/Scripts/TestRotator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotator : MonoBehaviour
+{
+    public Vector3 rotate;
+    public float x;
+    public float z;
+    public Vector3 view;
+    public GameObject cam;
+    public Vector3 camlook;
+    public float sensetivety;
+    public GameObject camPos;
+    public Vector3 camPosStart;
+    // Use this for initialization
+    void Start()
+    {
+        camPosStart = camPos.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camPos.transform.localPosition = camPosStart;
+        view.y = Input.GetAxis("Mouse X");
+        camlook.x = -Input.GetAxis("Mouse Y");
+        transform.Rotate(view * sensetivety);
+        cam.transform.Rotate(camlook * sensetivety);
+        if (cam.transform.eulerAngles.x > 60)
+        {
+            if (cam.transform.eulerAngles.x < 180)
+            {
+                cam.transform.eulerAngles = new Vector3(59.9999f, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+            }
+        }
+        if (cam.transform.eulerAngles.x < 300)
+        {
+            if (cam.transform.eulerAngles.x > 180)
+            {
+                cam.transform.eulerAngles = new Vector3(300.0001f, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+            }
+        }
+
+        cam.transform.eulerAngles = new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, 0);
+        RaycastHit hit;
+        if (Physics.Raycast(new Ray(transform.position, -(transform.position - camPos.transform.position)), out hit, Vector3.Distance(transform.transform.position, camPos.transform.position), ~(1 >> 10)))
+        {
+            if (hit.transform.gameObject.layer != 10)
+            {
+                if (hit.transform.gameObject.layer != 2)
+                {
+                    if (hit.transform.tag != "Player")
+                    {
+                        transform.position = hit.point;
+                        transform.position += transform.forward;
+                    }
+                }
+            }
+        }
+        if (Physics.Raycast(new Ray(camPos.transform.position, -(camPos.transform.position - transform.position)), out hit, Vector3.Distance(camPos.transform.transform.position, transform.position), ~(1 >> 10)))
+        {
+            if (hit.transform.gameObject.layer != 10)
+            {
+                if (hit.transform.gameObject.layer != 2)
+                {
+                    if (hit.transform.tag != "Player")
+                    {
+                        transform.position = hit.point;
+                        transform.position += transform.forward;
+                    }
+                }
+            }
+        }
+    }
+}
+    
