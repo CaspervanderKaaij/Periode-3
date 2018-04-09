15d34b5eaddf2ada21e179474a620d342fbe86f0
Levindye <33858994+Levindye@users.noreply.github.com>
2018-04-09 13:14:12 +0200

    Merge branch 'master' of https://github.com/CaspervanderKaaij/Periode-3

---
+++ Periode3Game/Assets/Stefan/Scripts/FadeIn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+    public GameObject options;
+    public GameObject canvas;
+    public GameObject[] faders;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < faders.Length; i++)
+        {
+            faders[i].GetComponent<Image>().enabled = false;
+            faders[i].GetComponent<Text>().enabled = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            for (int i = 0; i < faders.Length; i++)
+            {
+                faders[i].GetComponent<Image>().enabled = true;
+                faders[i].GetComponent<Text>().enabled = true;
+            }
+
+            gameObject.GetComponent<FadeIn>().enabled = false;
+        }
+	}
+}
---
+++ Periode3Game/Assets/Stefan/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button start;
+    public Button options;
+    public Button exit;
+
+	// Use this for initialization
+	void Start () {
+        start.onClick.AddListener(StartButton);
+        options.onClick.AddListener(OptionsButton);
+        exit.onClick.AddListener(ExitButton);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void StartButton()
+    {
+        print("Start");
+    }
+
+    public void OptionsButton()
+    {
+        print("Options");
+    }
+
+    public void ExitButton()
+    {
+        print("Exit");
+    }
+}
