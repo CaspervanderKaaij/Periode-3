717ee8f291ac760b1ca2663e2b3a0c753e040557
Stefan <stefanherder2001@gmail.com>
2018-04-10 15:22:39 +0200

    Debugging

--- Periode3Game/Assets/Stefan/Scripts/Hub/DialogueScript.cs
+++ Periode3Game/Assets/Stefan/Scripts/Hub/DialogueScript.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class DialogueScript : MonoBehaviour {
 
+    public ParticleSystem p;
+    public GameObject ship;
+
     public string[] dia;
     public float textSpeed;
     public Text text;
@@ -13,15 +16,22 @@ public class DialogueScript : MonoBehaviour {
 
     public Camera cam;
     public bool screenshake;
+    public Vector3 center;
+    public float shakeStrength;
 
 	// Use this for initialization
 	void Start () {
         StartCoroutine(DiaTimer(1));
+        center = cam.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(screenshake == true)
+        {
+            cam.transform.position = center + new Vector3(Random.Range(shakeStrength, -shakeStrength), Random.Range(shakeStrength, -shakeStrength), Random.Range(shakeStrength, -shakeStrength));
+            shakeStrength += 0.001f;
+        }
 	}
 
     public IEnumerator DiaTimer(float t)
@@ -30,9 +40,17 @@ public class DialogueScript : MonoBehaviour {
         text.text = dia[lineNumber];
         lineNumber++;
         StartCoroutine(DiaTimer(textSpeed));
-        if(lineNumber >= (dia.Length / 2))
+        if(lineNumber >= (dia.Length / 3))
         {
             screenshake = true;
+        }
+        if(lineNumber >= (dia.Length - (dia.Length / 4)))
+        {
+            p.Play(true);
+        }
+        if (lineNumber >= (dia.Length - (dia.Length / 8)))
+        {
+            ship.SetActive(false);
         }
         if (lineNumber >= dia.Length)
         {
