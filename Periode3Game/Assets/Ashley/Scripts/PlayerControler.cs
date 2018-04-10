42e1900eb062abae574cb9d0049a4816636b0cfa
AshleyvanderVelden <33858983+AshleyvanderVelden@users.noreply.github.com>
2018-04-10 12:39:44 +0200

    scripts voor puzzle
    scripts voor puzzle toegevoegt

---
+++ Periode3Game/Assets/Ashley/Scripts/PlayerControler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+    public Vector3 v;
+    public float hor;
+    public float ver;
+    public float walkSpeed;
+    public Vector3 vectorRotate;
+    public float rotateSpeed;
+    public float jumpSpeed;
+    public bool mayJump;
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {   
+        vectorRotate.y = Input.GetAxis("Mouse X");
+
+        transform.Rotate(vectorRotate * Time.deltaTime * rotateSpeed);
+
+        transform.Translate(v / walkSpeed * Time.deltaTime);
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+        v.x = hor;
+        v.z = ver;
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody>().velocity += jumpSpeed * Vector3.up;
+        }
+    }
+}
