e7745a5f5ad36330d1d568be390c5f279b14ad42
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2018-04-11 14:51:18 +0200

    Ace battle import
    Animaties en zo..

---
+++ Periode3Game/Assets/Ashley/Scripts/Incredients.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Incredients : MonoBehaviour {
+    public int buff;
+    public bool incredientOne;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void PickUp()
+    {
+        Destroy(gameObject);
+        incredientOne = true;
+    }
+
+}
---
+++ Periode3Game/Assets/Ashley/Scripts/PlatformController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+    public Vector3 route;
+    public float moveSpeedUp;
+    public float moveSpeedDown;
+    public bool upQuestionmark;
+    public int waitSec;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (upQuestionmark == false)
+        {
+            StartCoroutine("Up");
+        }
+        if(upQuestionmark == true)
+        {
+            StartCoroutine("Down");
+        }
+    }
+    IEnumerator Up()
+    {
+        transform.Translate(route * Time.deltaTime * moveSpeedUp);
+        yield return new WaitForSeconds(waitSec);
+        upQuestionmark = true;
+    }
+    IEnumerator Down()
+    {
+        transform.Translate(route * Time.deltaTime * moveSpeedDown);
+        yield return new WaitForSeconds(waitSec);
+        upQuestionmark = false;
+    }
+}
