cabb7ca6d4de72ce1e44a70fad203fe3913894dc
Stefan <stefanherder2001@gmail.com>
2018-04-22 18:19:22 +0200

    Inter Character System
    + Characters from hub go to Overworld
    (Animations not working yet)

---
+++ Periode3Game/Assets/Levi(Indye)/Scripts/CharacterChanger.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterChanger : MonoBehaviour {
+
+    public GameObject c1;
+    public GameObject c2;
+    public GameObject c3;
+    public GameObject c4;
+    public GameObject movementHolder;
+    public int character;
+
+    void Start () {
+        if (PlayerPrefs.GetInt("Character") == 1)
+        {
+            First();
+        }
+        else
+        {
+            if (PlayerPrefs.GetInt("Character") == 2)
+            {
+                Second();
+            }
+            else
+            {
+                if (PlayerPrefs.GetInt("Character") == 3)
+                {
+                    Third();
+                }
+                else
+                {
+                    if (PlayerPrefs.GetInt("Character") == 4)
+                    {
+                        Fourth();
+                    }
+                }
+            }
+        }
+    }
+
+    
+
+    public void First()
+    {
+        c1.SetActive(true);
+        c2.SetActive(false);
+        c3.SetActive(false);
+        c4.SetActive(false);
+        c1.GetComponent<Animator>().Play("anal");
+        character = 1;
+    }
+
+    public void Second()
+    {
+        c1.SetActive(false);
+        c2.SetActive(true);
+        c3.SetActive(false);
+        c4.SetActive(false);
+        c2.GetComponent<Animator>().Play("anal");
+        character = 2;
+    }
+
+    public void Third()
+    {
+        c1.SetActive(false);
+        c2.SetActive(false);
+        c3.SetActive(true);
+        c4.SetActive(false);
+        c3.GetComponent<Animator>().Play("anal");
+        character = 3;
+    }
+
+    public void Fourth()
+    {
+        c1.SetActive(false);
+        c2.SetActive(false);
+        c3.SetActive(false);
+        c4.SetActive(true);
+        c4.GetComponent<Animator>().Play("anal");
+        character = 4;
+    }
+}
