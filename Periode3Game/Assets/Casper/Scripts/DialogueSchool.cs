e9eca6999205dcc25a520eae5d36d4911acbcc21
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2018-04-27 12:00:31 +0200

    Dialogue voor Programming
    Niets dat te maken heeft met de game maar ik heb voor programming een tweede dialogue system snel gemaakt.
    Ik hoop op een goed. Hoop, ik kon het niet pollishen at all.

---
+++ Periode3Game/Assets/Casper/Scripts/DialogueSchool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueSchool : MonoBehaviour
+{
+
+    public string[] dialogue;
+    public int curText;
+    public Text txt;
+
+    void Start()
+    {
+		curText = 1;
+        UpdateText();
+    }
+    void Update()
+    {
+		if (Input.anyKeyDown)
+        {
+			Answer();
+		}
+    }
+
+	public virtual void Answer(){
+            switch (Input.inputString)
+            {
+
+                case "2":
+                    curText *= 2;
+                    UpdateText();
+                    break;
+                case "1":
+                    curText *= 2;
+                    curText += 1;
+                    UpdateText();
+                    break;
+            }
+	}
+
+    public void UpdateText()
+    {
+        txt.text = dialogue[curText - 1];
+    }
+}
